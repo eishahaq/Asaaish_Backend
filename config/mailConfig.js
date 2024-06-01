27d97bea282743de10b560d9e53ce861c95ffc97
Syed Jazil Ali <74674793+jazil10@users.noreply.github.com>
2024-06-01 08:19:00 +0500

    Update mailConfig.js

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -1,12 +1,12 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-// Configure Nodemailer to use Hotmail
-const transporter = nodemailer.createTransport({
-    service: 'hotmail', // Use the 'hotmail' service
-    auth: {
-        user: 'classic.airmated@hotmail.com', // Replace with your Hotmail email
-        pass: '0300360814455' // Replace with your Hotmail password
-    }
-});
+// // Configure Nodemailer to use Hotmail
+// const transporter = nodemailer.createTransport({
+//     service: 'hotmail', // Use the 'hotmail' service
+//     auth: {
+//         user: 'classic.airmated@hotmail.com', // Replace with your Hotmail email
+//         pass: '0300360814455' // Replace with your Hotmail password
+//     }
+// });
 
-module.exports = transporter;
+// module.exports = transporter;
